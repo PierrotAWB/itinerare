8284c2a1a3e2b0a2609c5e3654e46989713d3fa0
PierrotAWB <andrewwang298@gmail.com>
2019-06-22 18:29:08 -0400

    Fix home page routing

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', ensureAuthenticated, function(req, res, next) {
-//   res.render('index', { title: 'Members' });
-// });
-exports.index=function(req,res) {
-	res.render('index', {title: 'ejs'}); };
+router.get('/', ensureAuthenticated, function(req, res, next) {
+  res.render('index', { title: 'Members' });
+});
 
 
 function ensureAuthenticated(req, res, next){
