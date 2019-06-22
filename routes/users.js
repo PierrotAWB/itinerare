bbcd1b185b99d1cee2b695f3ecd2419fb52f0ea7
PierrotAWB <andrewwang298@gmail.com>
2019-06-22 13:45:00 -0400

    Add login stuff

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
+
+var User = require('../models/users');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register',{title:'Register'});
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {title:'Login'});
+});
+
+router.post('/register', upload.single('profileimage') ,function(req, res, next) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  if(req.file){
+  	console.log('Uploading File...');
+  	var profileimage = req.file.filename;
+  } else {
+  	console.log('No File Uploaded...');
+  	var profileimage = 'noimage.jpg';
+  }
+
+  var newUser = new User({
+  	name: name,
+  	email: email,
+  	username: username,
+  	password: password,
+  	profileimage: profileimage
+  });
+
+  User.createUser(newUser, function(err, user) {
+  	if (err) throw err
+  	console.log(user);
+  });
+
+  res.location('/');
+  res.redirect('/');
 });
 
 module.exports = router;
