0f170fcd573dd4a8d4b41eedce5c405dd1e62f42
PierrotAWB <andrewwang298@gmail.com>
2019-06-22 16:12:20 -0400

    Add Bulma

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', ensureAuthenticated, function(req, res, next) {
-  res.render('index', { title: 'Members' });
-});
+// router.get('/', ensureAuthenticated, function(req, res, next) {
+//   res.render('index', { title: 'Members' });
+// });
+exports.index=function(req,res) {
+	res.render('index', {title: 'ejs'}); };
+
 
 function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
