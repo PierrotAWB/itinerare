74aa246eb1ea417b4b8794812f353a23fea591f6
PierrotAWB <andrewwang298@gmail.com>
2019-06-22 22:48:24 -0400

    Create home screen with personalized welcome message

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ router.get('/', ensureAuthenticated, function(req, res, next) {
   res.render('index', { title: 'Members' });
 });
 
-
 function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
