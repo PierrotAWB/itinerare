24447c8332d6c2455cf5af27d1521500fc2d76df
tylerdsilva <tylerdsilva1@gmail.com>
2019-08-13 23:38:20 -0400

    Added the routing for the project

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,15 +3,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BarComponent } from './bar/bar.component';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarComponent
+    BarComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
